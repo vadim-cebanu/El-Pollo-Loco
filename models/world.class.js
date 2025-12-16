d5450e7c264dd85f65b97370dbf822993282995f
vadyk <vadykdeejay@gmail.com>
2025-12-16 20:55:41 +0100

    Cloud class added

--- models/world.class.js
+++ models/world.class.js
@@ -6,6 +6,9 @@ class World {
     new Chicken(),
     new Chicken()
 ];
+clouds =[
+    new Cloud()
+]
 canvas;
 ctx;
 
@@ -24,6 +27,12 @@ constructor(canvas){
             this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
            this.enemies.forEach(enemy => {
                             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+
+            });
+
+
+               this.clouds.forEach(cloud => {
+                     this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
 
             });
 
