62b468abff25cf7088baf125a34736085fab3c5d
vadyk <vadykdeejay@gmail.com>
2025-12-16 22:01:09 +0100

    Background added

--- models/world.class.js
+++ models/world.class.js
@@ -8,7 +8,10 @@ class World {
 ];
 clouds =[
     new Cloud()
-]
+];
+backgroundObjects = [
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100)
+];
 canvas;
 ctx;
 
@@ -24,23 +27,28 @@ constructor(canvas){
 
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-           this.enemies.forEach(enemy => {
-                            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-
-            });
-
-
-               this.clouds.forEach(cloud => {
-                     this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-
-            });
-
-
-
+           
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
+           
+        
             let self = this;
             requestAnimationFrame(function(){
                 self.draw();
             });
+    }
+
+addObjectsToMap(objects){
+    objects.forEach(o => {
+        this.addToMap(o);
+
+    });
+}
+
+    addToMap(mo){
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+
     }
 }
