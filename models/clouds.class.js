6077a5929d0e20a5a531dd998df8673e11cc2ff7
vadyk <vadykdeejay@gmail.com>
2025-12-17 08:56:09 +0100

    Backgroung added, starting animate

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -11,5 +11,12 @@ width= 500;
 
         this.x= 0 + Math.random() * 500;
 
+        this.animate();
+}
+
+animate(){
+    setInterval(() => {
+    this.x -=0.15;
+}, 1000/60);
 }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -10,7 +10,11 @@ clouds =[
     new Cloud()
 ];
 backgroundObjects = [
-    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 100)
+    new BackgroundObject('img/5_background/layers/air.png',0),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0,),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png',0),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png',0)
+
 ];
 canvas;
 ctx;
@@ -28,10 +32,11 @@ constructor(canvas){
     draw(){
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
            
+               
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
-        this.addObjectsToMap(this.backgroundObjects);
            
         
             let self = this;
