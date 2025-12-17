380dccf167d82d5e63c2e9fd70337954cddda365
vadyk <vadykdeejay@gmail.com>
2025-12-17 10:05:55 +0100

    Working with animation

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,12 +4,23 @@ class MovableObject{
     img;
     height = 150;
     width = 100;
+imageCache = [];
+currentImage=0;
+
+
 
 loadImage(path){
     this.img = new Image();
     this.img.src = path;
 }
 
+    loadImages(arr){
+        arr.forEach((path) => {
+        let img = new Image();
+        img.src = path;
+       this.imageCache[path]=img;
+        });
+    }
 
     moveRight(){
         console.log('moving right');
