57d94f6556a6e3c20a92d5f30218758934af360a
vadyk <vadykdeejay@gmail.com>
2025-12-17 13:17:50 +0100

    New class added

--- models/character.class.js
+++ models/character.class.js
@@ -1,37 +1,53 @@
-class Character extends MovableObject{
+class Character extends MovableObject {
 
-height=280;
-y=155;
-IMAGES_WALKING=[
-                         'img/2_character_pepe/2_walk/W-21.png',
-                         'img/2_character_pepe/2_walk/W-22.png',
-                         'img/2_character_pepe/2_walk/W-23.png',
-                         'img/2_character_pepe/2_walk/W-24.png',
-                         'img/2_character_pepe/2_walk/W-25.png',
-                         'img/2_character_pepe/2_walk/W-26.png'
-        ];
-world;
-constructor(){
-    super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+    height = 280;
+    y = 155;
+    speed=10;
+    IMAGES_WALKING = [
+        'img/2_character_pepe/2_walk/W-21.png',
+        'img/2_character_pepe/2_walk/W-22.png',
+        'img/2_character_pepe/2_walk/W-23.png',
+        'img/2_character_pepe/2_walk/W-24.png',
+        'img/2_character_pepe/2_walk/W-25.png',
+        'img/2_character_pepe/2_walk/W-26.png'
+    ];
+    world;
+    constructor() {
+        super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
 
         this.animate();
-}
-        animate(){
-            setInterval(()  => {
+    }
+    animate() {
 
-                if(this.world.keyboard.RIGHT){
+        setInterval(() =>{
+            if (this.world.keyboard.RIGHT) {
+             this.x += this.speed;
+             this.otherDirection = false;
+            }
 
+             if (this.world.keyboard.LEFT){
+             this.x -=this.speed;
+             this.otherDirection= true;
+            }
+            this.world.camera_x = -this.x;
+        },1000/60);
+
+
+
+        setInterval(() => {
+
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT ){
                 let i = this.currentImage % this.IMAGES_WALKING.length;
-           let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage ++;
-                }
-        },100);
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 100);
     }
 
 
-    jump(){
+    jump() {
 
 
     }
