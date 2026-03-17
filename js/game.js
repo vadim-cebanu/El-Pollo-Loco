2dd74fea01af51543aba24dac922d24f7f57281b
vadyk <vadykdeejay@gmail.com>
2026-03-17 10:44:44 +0100

    Implement asset preloading system for faster game start
    Add loading screen with progress indicator that preloads all game assets (images) before the start button becomes available. This eliminates lag when starting the game from FTP.
    
    Changes:
    - Add loading spinner and progress text to start screen (HTML)
    - Implement getAllImagePaths() to list all ~150 game images
    - Implement preloadAssets() to load images asynchronously with progress tracking
    - Add CSS animations for loading spinner
    - Hide start buttons until all assets are loaded
    - Show loading progress from 0% to 100%
    
    Benefits:
    - Loading happens on page load instead of on 'Start Game' click
    - Eliminates 2-5 second lag when starting the game
    - Game starts instantly after clicking 'Start Game'
    - Better user experience with visual loading feedback

--- js/game.js
+++ js/game.js
@@ -8,6 +8,8 @@ let keyboard = new Keyboard();
 let audioManager;
 /** @type {boolean} */
 let gameRunning = false;
+/** @type {boolean} */
+let assetsLoaded = false;
 
 /**
  * Initializes the game on page load
@@ -20,6 +22,202 @@ function init() {
   setupMobileControls();
   setupKeyboardListeners();
   handleResponsive();
+  preloadAssets();
+}
+
+/**
+ * Gets all image paths used in the game
+ * @returns {string[]} Array of image paths
+ */
+function getAllImagePaths() {
+  return [
+    // Character
+    "img/2_character_pepe/1_idle/idle/I-1.png",
+    "img/2_character_pepe/1_idle/idle/I-2.png",
+    "img/2_character_pepe/1_idle/idle/I-3.png",
+    "img/2_character_pepe/1_idle/idle/I-4.png",
+    "img/2_character_pepe/1_idle/idle/I-5.png",
+    "img/2_character_pepe/1_idle/idle/I-6.png",
+    "img/2_character_pepe/1_idle/idle/I-7.png",
+    "img/2_character_pepe/1_idle/idle/I-8.png",
+    "img/2_character_pepe/1_idle/idle/I-9.png",
+    "img/2_character_pepe/1_idle/idle/I-10.png",
+    "img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "img/2_character_pepe/1_idle/long_idle/I-16.png",
+    "img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "img/2_character_pepe/1_idle/long_idle/I-20.png",
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-33.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
+    "img/2_character_pepe/4_hurt/H-41.png",
+    "img/2_character_pepe/4_hurt/H-42.png",
+    "img/2_character_pepe/4_hurt/H-43.png",
+    "img/2_character_pepe/5_dead/D-51.png",
+    "img/2_character_pepe/5_dead/D-52.png",
+    "img/2_character_pepe/5_dead/D-53.png",
+    "img/2_character_pepe/5_dead/D-54.png",
+    "img/2_character_pepe/5_dead/D-55.png",
+    "img/2_character_pepe/5_dead/D-56.png",
+    "img/2_character_pepe/5_dead/D-57.png",
+    "img/2_character_pepe/scarry/1.png",
+    "img/2_character_pepe/scarry/2.png",
+    "img/2_character_pepe/scarry/3.png",
+    "img/2_character_pepe/scarry/4.png",
+    "img/2_character_pepe/scarry/5.png",
+    "img/2_character_pepe/scarry/6.png",
+    "img/2_character_pepe/scarry/7.png",
+    // Chicken
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+    "img/3_enemies_chicken/chicken_normal/2_dead/dead.png",
+    // Small Chicken
+    "img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
+    "img/3_enemies_chicken/chicken_small/2_dead/dead.png",
+    // Endboss
+    "img/4_enemie_boss_chicken/1_walk/G1.png",
+    "img/4_enemie_boss_chicken/1_walk/G2.png",
+    "img/4_enemie_boss_chicken/1_walk/G3.png",
+    "img/4_enemie_boss_chicken/1_walk/G4.png",
+    "img/4_enemie_boss_chicken/2_alert/G5.png",
+    "img/4_enemie_boss_chicken/2_alert/G6.png",
+    "img/4_enemie_boss_chicken/2_alert/G7.png",
+    "img/4_enemie_boss_chicken/2_alert/G8.png",
+    "img/4_enemie_boss_chicken/2_alert/G9.png",
+    "img/4_enemie_boss_chicken/2_alert/G10.png",
+    "img/4_enemie_boss_chicken/2_alert/G11.png",
+    "img/4_enemie_boss_chicken/2_alert/G12.png",
+    "img/4_enemie_boss_chicken/3_attack/G13.png",
+    "img/4_enemie_boss_chicken/3_attack/G14.png",
+    "img/4_enemie_boss_chicken/3_attack/G15.png",
+    "img/4_enemie_boss_chicken/3_attack/G16.png",
+    "img/4_enemie_boss_chicken/3_attack/G17.png",
+    "img/4_enemie_boss_chicken/3_attack/G18.png",
+    "img/4_enemie_boss_chicken/3_attack/G19.png",
+    "img/4_enemie_boss_chicken/3_attack/G20.png",
+    "img/4_enemie_boss_chicken/4_hurt/G21.png",
+    "img/4_enemie_boss_chicken/4_hurt/G22.png",
+    "img/4_enemie_boss_chicken/4_hurt/G23.png",
+    "img/4_enemie_boss_chicken/5_dead/G24.png",
+    "img/4_enemie_boss_chicken/5_dead/G25.png",
+    "img/4_enemie_boss_chicken/5_dead/G26.png",
+    // Backgrounds
+    "img/5_background/layers/air.png",
+    "img/5_background/layers/3_third_layer/1.png",
+    "img/5_background/layers/3_third_layer/2.png",
+    "img/5_background/layers/2_second_layer/1.png",
+    "img/5_background/layers/2_second_layer/2.png",
+    "img/5_background/layers/1_first_layer/1.png",
+    "img/5_background/layers/1_first_layer/2.png",
+    "img/5_background/layers/4_clouds/1.png",
+    "img/5_background/layers/4_clouds/2.png",
+    // Bottles
+    "img/6_salsa_bottle/salsa_bottle.png",
+    "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
+    "img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
+    "img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
+    // Status bars
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png",
+    "img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue0.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue20.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue40.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue60.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue80.png",
+    "img/7_statusbars/2_statusbar_endboss/blue/blue100.png",
+    // Coins
+    "img/8_coin/coin_1.png",
+    "img/8_coin/coin_2.png",
+    // Intro/Outro screens
+    "img/9_intro_outro_screens/start/startscreen_1.png",
+    "img/9_intro_outro_screens/start/startscreen_2.png",
+    "img/You won, you lost/You Win A.png",
+    "img/You won, you lost/Game Over.png",
+  ];
+}
+
+/**
+ * Preloads all game assets
+ * @returns {Promise<void>}
+ */
+async function preloadAssets() {
+  const imagePaths = getAllImagePaths();
+  const loadingText = document.getElementById("loading-text");
+  let loadedCount = 0;
+  const totalAssets = imagePaths.length;
+
+  const loadImage = (src) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => resolve(img); // Continue even if image fails
+      img.src = src;
+    });
+  };
+
+  for (const path of imagePaths) {
+    await loadImage(path);
+    loadedCount++;
+    const progress = Math.round((loadedCount / totalAssets) * 100);
+    loadingText.textContent = `Loading assets... ${progress}%`;
+  }
+
+  assetsLoaded = true;
+  showStartButtons();
+}
+
+/**
+ * Shows start buttons after assets are loaded
+ * @returns {void}
+ */
+function showStartButtons() {
+  document.getElementById("loading-indicator").classList.add("hidden");
+  document.getElementById("start-buttons").classList.remove("hidden");
 }
 
 /**
